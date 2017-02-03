6da083855b35533e060abf41ecf825503cf4edf4
petermccloud <peter@mccloudaero.com>
2017-02-03 12:15:37 -0800

    upgrades to rotor.py to use splines for airfoil data

--- rbea/rotor.py
+++ rbea/rotor.py
@@ -1,4 +1,7 @@
+import sys
 import math
+import numpy as np
+import scipy.interpolate
 
 # Rotor Blade Element Analysis
 # Python Script for determining Rotor Performance using Blade Element Theory
@@ -44,35 +47,38 @@ def airfoil_coef(alpha):
 
 pi = math.pi
 
-# Rotor Inputs (metric)
-diameter = .9144	# meters
-#diameter = 1.016	# meters
-blades = 2.0		# number of blades
-root_chord = 0.1143	# meters
-tip_chord = 0.1016	# meters
-root_theta = 18.0	# degrees
-tip_theta = 5.0		# degrees
-#tip_theta = 5.0	# degrees
-RPMs = [2200,2310,2420,2640,2820,3080,3300,3410,3520,3740,3960]		# rotor speed in RPM
+# Read inputs file
+execfile('inputs.txt')
 
-# Atmospheric Inputs
-# Note: rho = 1.225 (kg/m3) standard sea level atmosphere density @ 70 F
-#rho = 1.1644	# Sea level pressure, 30 C
-rho = 1.225	# Sea level pressure, 30 C
+# Read airfoil data
+airfoil = np.loadtxt('./airfoils/'+airfoil_name+'.dat',skiprows=1,delimiter=',')
+# Create splines
+mach_data = airfoil[:,0]
+alpha_data = airfoil[:,1]
+cd_data = airfoil[:,2]
+cl_data = airfoil[:,3]
+# Determine Mach Range
+if min(mach_data) == max(mach_data):
+ # Single Mach Number present, Create 1D splines
+ airfoil_data_type = '1D'
+ cd_spline = scipy.interpolate.splrep(alpha_data,cd_data)
+ cl_spline = scipy.interpolate.splrep(alpha_data,cl_data)
+else:
+ # Multiple Mach Numbers prsent, Create 2D splines
+ airfoil_data_type = '2D'
+ cd_spline = scipy.interpolate.bisplrep(mach_data,alpha_data,cd_data)
+ cl_spline = scipy.interpolate.bisplrep(mach_data,alpha_data,cl_data)
 
 # Initial Calculations
 tip_radius = diameter/2
-#root_radius = 0.1*tip_radius
-root_radius = 0.1143
 area = pi*diameter*diameter/4
 avg_chord = (root_chord + tip_chord)/2
 sigma=2*avg_chord/2/pi/tip_radius			# solidity
 
 # Blade Element Calculation
-elements = 20
-r_inc = (tip_radius-root_radius)/(elements-1)
-theta_inc = (tip_theta-root_theta)/(elements-1)
-chord_inc = (tip_chord-root_chord)/(elements-1)
+r_inc = (tip_radius-root_radius)/(num_elements-1)
+theta_inc = (tip_theta-root_theta)/(num_elements-1)
+chord_inc = (tip_chord-root_chord)/(num_elements-1)
 
 # Initialize lists
 radius = []
@@ -84,17 +90,27 @@ thrust = []
 drag =[]
 torque = []
 
+# Initialize total_output file
+total_output = open('./output.txt','w')
+# Print Data Header
+total_output.write('RPM\tT(N)\tP(kW)\tT(lbf)\tP(Hp)\tFM\tM_tip\n')
+  
+# Initialize total_output file
+radial_output = open('./radial_output.txt','w')
+# Print Data Header
+radial_output.write('i\\RPM\t')
+for i in range(num_elements):
+ radial_output.write(str(i)+'\t')
+radial_output.write('\n')
+
 # Populate radii and theta lists
-for i in range(elements):
+for i in range(num_elements):
  radius.append(root_radius+i*r_inc)
  chord.append(root_chord+chord_inc)
  theta.append(root_theta+i*theta_inc)
  theta_rad.append(theta[i]/180*pi)
  alpha_rad.append(0)
 
-# Print Data Header
-print 'RPM\tT(N)\tP(kW)\tT(lbf)\tP(Hp)\tFM'
-  
 for RPM in RPMs:
  # Initial Calcs at RPM
  n = RPM/60			# RPS
@@ -106,10 +122,10 @@ for RPM in RPMs:
  drag =[]
  torque = []
 
- for i in range(elements):
+ for i in range(num_elements):
   # Guess at initial values of inflow and swirl factor
   # Note: swirl currently isn't used
-  v_inflow = 1
+  v_inflow = 10
   #v_swirl = 0.1
     
   # Iterate at Each Blade Element
@@ -117,13 +133,26 @@ for RPM in RPMs:
   finished = False
   while( finished == False):
    v_axial = v_inflow				# axial velocity
-   #v_radial = omega*radius[i] - v_swirl	# disk plane velocity
    v_radial = omega*radius[i]			# disk plane velocity
+   #v_radial = omega*radius[i] - v_swirl	# disk plane velocity
+   v_mag = pow((v_axial*v_axial+v_radial*v_radial),0.5)	# local velocity at blade (Resultant of Induced & Radial)
+   mach = v_mag/a
    phi_rad = math.atan2(v_axial,v_radial)	# flow angle (radians)
    alpha_rad[i] = theta_rad[i]- phi_rad		# blade angle of attack
-   cl,cd = airfoil_coef(alpha_rad[i])
+   #cl,cd = airfoil_coef(alpha_rad[i])
+   alpha = alpha_rad[i]*180/pi
+   alpha = min(alpha,12)
+
+   # Find section coefficients
+   if airfoil_data_type == '1D':
+    cd = scipy.interpolate.splev(alpha,cd_spline) 
+    cl = scipy.interpolate.splev(alpha,cl_spline) 
+   elif airfoil_data_type == '2D':
+    cd = scipy.interpolate.bisplev(mach,alpha,cd_spline) 
+    cl = scipy.interpolate.bisplev(mach,alpha,cl_spline) 
+   # Appply drag factor
+   cd = cd*drag_factor   
    
-   v_mag = pow((v_axial*v_axial+v_radial*v_radial),0.5)	# local velocity at blade (Resultant of Induced & Radial)
    q = 0.5*rho*pow(v_mag,2)								# local dynamic pressure
    DtDr = q*blades*chord[i]*(cl*math.cos(phi_rad)-cd*math.sin(phi_rad))			# thrust grading
    DdDr = q*blades*chord[i]*(cd*math.cos(phi_rad)+cl*math.sin(phi_rad))			# drag grading
@@ -137,7 +166,7 @@ for RPM in RPMs:
    iter += 1
   
    # check for convergence
-   if ( math.fabs(v_inflow_new-v_inflow)< 0.005):
+   if ( math.fabs(v_inflow_new-v_inflow)< 0.001):
     finished = True
    # check to see if iteration stuck
    elif(iter>2000):
@@ -153,12 +182,15 @@ for RPM in RPMs:
   drag.append(DdDr*r_inc)
   torque.append(DqDr*r_inc)
 
+  #print RPM,i,iter
+
  # Convert radians to degrees
- for i in range(elements):
+ radial_output.write('{:d}\t'.format(RPM))
+ for i in range(num_elements):
   theta[i] = theta[i]*180/pi
   alpha_rad[i] = alpha_rad[i]*180/pi
-  #print i,alpha_rad[i]
-
+  radial_output.write('{:5.3f}\t'.format(alpha_rad[i]))
+ radial_output.write('\n')
  
  # Totals
  T = sum(thrust)			# total thrust (N) 
@@ -180,4 +212,10 @@ for RPM in RPMs:
  P_imp = P*0.00134102209*1000	# kW to Hp
  
  FM = P_ideal/P
- print RPM,'\t',round(T,2),'\t',round(P,2),'\t',round(T_imp,2),'\t',round(P_imp,2),'\t',round(FM,3)
+ M_tip = v_mag/a
+ total_output.write('{:d}\t{:5.2f}\t{:5.2f}\t{:5.2f}\t{:5.2f}\t{:5.3f}\t{:5.3f}\n'.format(RPM,T,P,T_imp,P_imp,FM,M_tip))
+
+
+# Close total_output
+total_output.close()
+radial_output.close()
