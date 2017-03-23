3360f4389bbe0b31484f68f97a996c8bcde3cae2
mccloudaero <peter@mccloudaero.com>
2017-03-22 20:03:50 -0700

    updated plot_airfoil and added moment plots

--- rbea/airfoils/plot_airfoil.py
+++ rbea/airfoils/plot_airfoil.py
@@ -17,6 +17,7 @@ mach_data = airfoil[:,0]
 alpha_data = airfoil[:,1]
 cd_data = airfoil[:,2]
 cl_data = airfoil[:,3]
+cm_data = airfoil[:,4]
 
 lift2drag_data = []
 num_values = len(cd_data)
@@ -33,14 +34,27 @@ if len(mach_values) == 1:
  cl_spline = scipy.interpolate.splrep(alpha_data,cl_data)
 
  # Plot Data 
- fig, axis_array = plt.subplots(3,sharex=True)
- axis_array[0].plot(alpha_data,cd_data,'o-',label='drag')
- axis_array[0].set_title('Drag')
- axis_array[1].plot(alpha_data,cl_data,'o-',label='lift')
- axis_array[1].set_title('Lift')
- axis_array[2].plot(alpha_data,lift2drag_data,'o-',label='lift to_drag')
- axis_array[2].set_title('Lift to Drag')
- plt.show() 
+ fig, axes_array = plt.subplots(3,sharex=True)
+ axes_array[0].plot(alpha_data,cd_data,'o-',label='drag')
+ axes_array[0].set_title('Drag')
+ axes_array[1].plot(alpha_data,cl_data,'o-',label='lift')
+ axes_array[1].set_title('Lift')
+ axes_array[2].plot(alpha_data,lift2drag_data,'o-',label='lift to_drag')
+ axes_array[2].set_title('Lift to Drag')
+ plt.show()
+ 
+ # CM
+ torque_fig = plt.figure()
+ axes = torque_fig.add_subplot(111)
+ sc = axes.scatter(alpha_data,cm_data,c=mach_data,vmin=0.3,vmax=0.5)
+ torque_fig.colorbar(sc)
+ axes.grid(True)
+ axes.set_xlabel('Alpha (deg)')
+ axes.set_ylabel('Pitch Coefficient')
+ axes.set_ylim(-0.1,0.1)
+ # Title
+ torque_fig.text(0.2,0.8, airfoil_name, fontsize=20)
+ plt.show()
 
 
 else:
@@ -51,16 +65,32 @@ else:
 
  # Plot 3D Data
  drag_fig = plt.figure()
- axis = drag_fig.add_subplot(111, projection='3d')
- axis.scatter(mach_data,alpha_data,cd_data)
+ axes = drag_fig.add_subplot(111, projection='3d')
+ axes.scatter(mach_data,alpha_data,cd_data)
  plt.show()
 
  lift_fig = plt.figure()
- axis = lift_fig.add_subplot(111, projection='3d')
- axis.scatter(mach_data,alpha_data,cl_data)
+ axes = lift_fig.add_subplot(111, projection='3d')
+ axes.scatter(mach_data,alpha_data,cl_data)
  plt.show()
 
  lift2drag_fig = plt.figure()
- axis = lift2drag_fig.add_subplot(111, projection='3d')
- axis.scatter(mach_data,alpha_data,lift2drag_data)
+ axes = lift2drag_fig.add_subplot(111, projection='3d')
+ axes.scatter(mach_data,alpha_data,lift2drag_data)
  plt.show()
+
+ # CM
+ torque_fig = plt.figure()
+ axes = torque_fig.add_subplot(111)
+ sc = axes.scatter(alpha_data,cm_data,c=mach_data,vmin=0.3,vmax=0.5)
+ cbar = torque_fig.colorbar(sc)
+ cbar.ax.set_ylabel('Mach')
+ axes.grid(True)
+ axes.set_xlabel('Alpha (deg)')
+ axes.set_ylabel('Moment Coefficient')
+ axes.set_ylim(-0.02,0.02)
+ # Title
+ torque_fig.text(0.2,0.8, airfoil_name, fontsize=20)
+ plt.show()
+
+ 
